5ec63bb8752dfdac27dfa9b88f7c08a2cdacec21
Greg Chamberlain <gregchamberlain94@gmail.com>
2016-08-11 00:54:17 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# React Component Skeleton
+
+## Instructions
+3. npm install
+1. Change project name/version in package.json
+3. Create you new Component
+
+## Development
+1. Export all components through src/index.js
+2. Import component into example/main.js
+3. Setup your example for development
+4. npm start
+
+## Publishing to npm
+1. Ensure correct information in package.json
+2. npm run build
+3. npm publish
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var getConfig = require('hjs-webpack');
+
+
+module.exports = getConfig({
+
+  isDev: process.env.NODE_ENV !== "production",
+  // entry point for the app
+  in: process.env.NODE_ENV !== "production"?'example/main.js':'src/index.js',
+
+  // Name or full path of output directory
+  // commonly named `www` or `public`. This
+  // is where your fully static site should
+  // end up for simple deployment.
+  out: 'dist',
+
+  output: {
+    filename: 'bundle.js'
+  },
+
+
+  // This will destroy and re-create your
+  // `out` folder before building so you always
+  // get a fresh folder. Usually you want this
+  // but since it's destructive we make it
+  // false by default
+  clearBeforeBuild: true
+});
