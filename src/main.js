700c98bb5b5354457d76d5d2761cbece5641506d
Greg Chamberlain <gregchamberlain94@gmail.com>
2016-12-01 18:24:26 -0800

    Updateded to v0.2.3

--- src/main.js
+++ src/main.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom'
 import Chips, { Chip, Styles } from 'react-chips'
-console.log(Chips)
-console.log(Chip)
-console.log(Styles)
 
 let items = ["Java", "Ruby", "Javascript", "C", "C++"]
 
-render(<Chips autoCompleteData={items}/>, document.getElementById("root"));
+render(<Chips suggestions={items}/>, document.getElementById("root"));
