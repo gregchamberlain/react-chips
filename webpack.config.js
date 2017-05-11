6339abd1d7c5e963923eef65d012d732f75cd98f
Greg Chamberlain <gregchamberlain94@gmail.com>
2017-05-10 21:01:09 -0400

    Added prop-types package to replace React.PropTypes

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  devtool: 'cheap-module-source-map',
+  devtool: 'eval',
   entry: [
     'react-hot-loader/patch',
     'webpack-hot-middleware/client',
