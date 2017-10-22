89b763c7c1ef9bbe1b7942176df870755ce3ebdc
Greg Chamberlain <gregchamberlain94@gmail.com>
2017-10-22 12:19:31 -0400

    Create build command

--- examples/Basic.js
+++ examples/Basic.js
@@ -3,7 +3,7 @@ import React from 'react';
 import MyComponent from 'src';
 
 const BasicExample = () => (
-  <MyComponent />
+  <MyComponent title="Basic Example" />
 );
 
 export default BasicExample;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const MyComponent = () => (
-  <h1>React Component Boilerplate</h1>
+const MyComponent = ({ title }) => (
+  <h2>{title}</h2>
 );
+
+MyComponent.propTypes = {
+  title: PropTypes.string.isRequired
+};
 
 export default MyComponent;
